727a91a3dd6efe42ac7c8011604ccaaec21daf1c
Williams <111031439+William3147076912@users.noreply.github.com>
2024-10-07 19:36:08 +0800

    初步完善了主界面fxml，还差controller

---
+++ src/main/java/com/scau/cfd/controller/BeginController.java
@@ -0,0 +1,11 @@
+package com.scau.cfd.controller;
+
+/**
+ * 这个类是：
+ *
+ * @author: William
+ * @date: 2024-10-06T00:09:23CST 00:09
+ * @description:
+ */
+public class BeginController {
+}
---
+++ src/main/java/com/scau/cfd/controller/MainController.java
@@ -0,0 +1,11 @@
+package com.scau.cfd.controller;
+
+/**
+ * 这个类是：
+ *
+ * @author: William
+ * @date: 2024-10-06T00:09:31CST 00:09
+ * @description:
+ */
+public class MainController {
+}
